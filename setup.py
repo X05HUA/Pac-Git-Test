40f545a15b3d2af58a0dddf972847e25b2a15c24
X05HUA <61294910+X05HUA@users.noreply.github.com>
2020-10-28 18:49:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with io.open(VERSION_PATH, 'r', encoding='utf-8') as f:
 
 setup(
     name = "mp4_to_mp3",        # what you want to call the archive/egg
-    packages=["mp4_to_mp3"],    # top-level python modules you can import like
+    # packages=["mp4_to_mp3"],    # top-level python modules you can import like
                                 #   'import foo'
     # dependency_links = [],      # custom links to a specific project
     # install_requires=[],
